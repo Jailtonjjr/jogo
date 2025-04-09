5186dbc1978b54d184cadc53bc1df55f083875fc
Jailtonjjr <128049513+Jailtonjjr@users.noreply.github.com>
2025-04-09 10:00:39 -0300

    Add files via upload

---
+++ Visual Studio 2022/jogo_cartas/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace jogo_cartas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imagem1 = new System.Windows.Forms.PictureBox();
+            this.imagem2 = new System.Windows.Forms.PictureBox();
+            this.imagem3 = new System.Windows.Forms.PictureBox();
+            this.imagem4 = new System.Windows.Forms.PictureBox();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagem1
+            // 
+            this.imagem1.Image = ((System.Drawing.Image)(resources.GetObject("imagem1.Image")));
+            this.imagem1.Location = new System.Drawing.Point(71, 125);
+            this.imagem1.Name = "imagem1";
+            this.imagem1.Size = new System.Drawing.Size(196, 289);
+            this.imagem1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imagem1.TabIndex = 0;
+            this.imagem1.TabStop = false;
+            this.imagem1.Click += imagem1_Click;
+            // 
+            // imagem2
+            // 
+            this.imagem2.Image = ((System.Drawing.Image)(resources.GetObject("imagem2.Image")));
+            this.imagem2.Location = new System.Drawing.Point(347, 125);
+            this.imagem2.Name = "imagem2";
+            this.imagem2.Size = new System.Drawing.Size(196, 289);
+            this.imagem2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imagem2.TabIndex = 1;
+            this.imagem2.TabStop = false;
+            this.imagem2.Click += imagem2_Click;
+            // 
+            // imagem3
+            // 
+            this.imagem3.Image = ((System.Drawing.Image)(resources.GetObject("imagem3.Image")));
+            this.imagem3.Location = new System.Drawing.Point(618, 125);
+            this.imagem3.Name = "imagem3";
+            this.imagem3.Size = new System.Drawing.Size(196, 289);
+            this.imagem3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imagem3.TabIndex = 2;
+            this.imagem3.TabStop = false;
+            this.imagem3.Click += imagem3_Click;
+            // 
+            // imagem4
+            // 
+            this.imagem4.Image = ((System.Drawing.Image)(resources.GetObject("imagem4.Image")));
+            this.imagem4.Location = new System.Drawing.Point(870, 125);
+            this.imagem4.Name = "imagem4";
+            this.imagem4.Size = new System.Drawing.Size(196, 289);
+            this.imagem4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imagem4.TabIndex = 3;
+            this.imagem4.TabStop = false;
+            this.imagem4.Click += imagem4_Click;
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(944, 25);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(113, 41);
+            this.btnReiniciar.TabIndex = 4;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            btnReiniciar.Click += BtnReiniciar_Click;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 574);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.imagem4);
+            this.Controls.Add(this.imagem3);
+            this.Controls.Add(this.imagem2);
+            this.Controls.Add(this.imagem1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagem1;
+        private System.Windows.Forms.PictureBox imagem2;
+        private System.Windows.Forms.PictureBox imagem3;
+        private System.Windows.Forms.PictureBox imagem4;
+        private System.Windows.Forms.Button btnReiniciar;
+    }
+}
+
---
+++ Visual Studio 2022/jogo_cartas/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogo_cartas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         InitializeComponent();
+            this.BackColor = System.Drawing.Color.LightGreen;
+            imagem1.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\tras_redimencionada.JPG");
+            imagem2.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\tras_redimencionada.JPG");
+            imagem3.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\tras_redimencionada.JPG");
+            imagem4.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\tras_redimencionada.JPG");
+        }
+        
+        // Evento para o clique do botão de reiniciar
+        //private void BtnReiniciar_Click(object sender, EventArgs e)
+        //{
+        //    // Fecha o formulário atual
+        //    this.Close();
+
+        //    // Reabre o formulário criando uma nova instância
+        //    Form1 novoForm = new Form1();
+        //    novoForm.Show();
+        //}
+        private void BtnReiniciar_Click(object sender, EventArgs e)
+        {
+            Application.Restart(); // Reinicia toda a aplicação
+        }
+
+        public static void coordenadas()
+        {
+            // As coordenadas possíveis
+            List<Point> coordenadas = new List<Point>
+            {
+            new Point(347, 125),
+            new Point(870, 125),
+            new Point(618, 125),
+            new Point(71, 125)
+            };
+
+            // As imagens (simulados como variáveis)
+            PictureBox imagem1 = new PictureBox();
+            PictureBox imagem2 = new PictureBox();
+            PictureBox imagem3 = new PictureBox();
+            PictureBox imagem4 = new PictureBox();
+
+            // Lista de imagens
+            List<PictureBox> imagens = new List<PictureBox> { imagem1, imagem2, imagem3, imagem4 };
+
+            // Gerar as permutações das posições
+            var Numeros = Quantidade(coordenadas);
+
+            // Aplicar cada permutação nas imagens
+            foreach (var num in Numeros)
+            {
+                Console.WriteLine("Nova combinação:");
+                for (int i = 0; i < imagens.Count; i++)
+                {
+                    imagens[i].Location = num[i];
+                    Console.WriteLine($"Imagem {i + 1} -> {imagens[i].Location}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        // Método para gerar as permutações de uma lista
+        public static List<List<Point>> Quantidade(List<Point> lista)
+        {
+            var resultado = new List<List<Point>>();
+            QuantidadeNumeros(lista, 0, resultado);
+            return resultado;
+        }
+
+        // Método recursivo para gerar as permutações
+        public static void QuantidadeNumeros(List<Point> lista, int indice, List<List<Point>> resultado)
+        {
+            if (indice == lista.Count)
+            {
+                resultado.Add(new List<Point>(lista));
+            }
+            else
+            {
+                for (int i = indice; i < lista.Count; i++)
+                {
+                    // Troca os elementos
+                    Trocar(lista, indice, i);
+                    QuantidadeNumeros(lista, indice + 1, resultado);
+                    Trocar(lista, indice, i); // Desfaz a troca para a próxima iteração
+                }
+            }
+        }
+
+
+        // Método para trocar os elementos da lista
+        public static void Trocar(List<Point> lista, int i, int j)
+        {
+            Point temp = lista[i];
+            lista[i] = lista[j];
+            lista[j] = temp;
+        }
+        private void imagem1_Click(object sender, EventArgs e)
+        {
+            // Troca a imagem ao clicar
+            imagem1.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\As_de_espadas.JPG");
+        }
+        private void imagem2_Click(object sender, EventArgs e)
+        {
+            // Troca a imagem ao clicar
+            imagem2.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\Rainha_de_espadas.JPG");
+        }
+        private void imagem3_Click(object sender, EventArgs e)
+        {
+            // Troca a imagem ao clicar
+            imagem3.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\Rei_de_espadas.JPG");
+        }
+        private void imagem4_Click(object sender, EventArgs e)
+        {
+            // Troca a imagem ao clicar
+            imagem4.Image = System.Drawing.Image.FromFile("C:\\Users\\jande\\OneDrive\\Documentos\\Visual Studio 2022\\Valete_de_espadas.JPG");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+    }
+}
+
+
+
+
+
+
+
+        
+
